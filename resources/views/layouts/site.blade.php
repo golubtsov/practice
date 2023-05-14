8eea4259240a1a89ed5c1c27e3fc16c272c5b635
golubtsov <nproger781@gmail.com>
2023-05-14 21:00:55 +0700

    favicon

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
+    <link rel="shortcut icon" href="{{asset('images/icons/favicon.png')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{asset('./public/css/app.scss')}}">
     @vite(['resources/css/app.scss', 'resources/js/app.jsx'])
 </head>
